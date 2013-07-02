2528ee0547bfbf5b5ab0598201336480b81f0613
msg555 <msg555@gmail.com>
2013-07-02 11:54:34 -0400

    tweaked congestion control defaults

--- src/congestion.c
+++ src/congestion.c
@@ -4,20 +4,23 @@
 
 void _mstream_congestion_init(struct congestion_info* cinfo) {
   cinfo->slow_start = 1;
-  cinfo->rtt = 500000;
-  cinfo->rtt_ssq = 1000000LL * 1000000LL;
+  cinfo->rtt = 200000;
+  cinfo->rtt_ssq = 400000LL * 400000LL;
   cinfo->spacing = cinfo->rtt / 10.0;
 }
 
 void _mstream_congestion_ack(struct congestion_info* cinfo, time_val rtt) {
   if(rtt) {
+    int shft = cinfo->slow_start ? 3 : 6;
+
     cinfo->spacing /= cinfo->rtt;
-    cinfo->rtt = ((cinfo->rtt << 6) - cinfo->rtt + rtt) >> 6;
+    cinfo->rtt = ((cinfo->rtt << shft) - cinfo->rtt + rtt) >> shft;
     if(cinfo->rtt == 0) {
       cinfo->rtt = 1;
     }
     cinfo->spacing *= cinfo->rtt;
-    cinfo->rtt_ssq = ((cinfo->rtt_ssq << 6) - cinfo->rtt_ssq + rtt * rtt) >> 6;
+    cinfo->rtt_ssq = ((cinfo->rtt_ssq << shft) -
+                        cinfo->rtt_ssq + rtt * rtt) >> shft;
   }
   if(cinfo->slow_start) {
     cinfo->spacing = 1.0 / (1.0 / cinfo->spacing + 1.0 / cinfo->rtt);
